4a89a6076deefafb61baf46a6c183d7e719f8ed5
Sebastien Aubin <spicheaubin@devolutions.net>
2019-11-13 14:05:11 -0500

    Fix font for android too

--- lib/openid_client_io.dart
+++ lib/openid_client_io.dart
@@ -48,12 +48,13 @@ class Authenticator {
               request.response.headers.set("Content-type", "text/html");
               request.response.writeln(
                   "<html>"
+                    "<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
                     "<body>"
                       "<div style='width: 100%; text-align: center; font-family: Arial, Helvetica, sans-serif'>"
                         "<img src='https://login.devolutions.com/op/assets/devolutions-logo.png' width='100%' alt='Devolutions'/>"
-                        "<span style='font-size: 120px'>Success!</span>"
+                        "<span style='font-size: 10vw'>Success!</span>"
                         "<div style='background-color:#d3f5e0; border-left: 10px solid #49fc8e'>"
-                          "<p style='padding: 10px; color:#616161; font-size: 40px'>You can now close this page!</p>"
+                          "<p style='padding: 10px; color:#616161; font-size: 6vw'>You can now close this page!</p>"
                         "</div>"
                       "</div>"
                     "</body>"
