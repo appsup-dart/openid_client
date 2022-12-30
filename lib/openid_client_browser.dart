65f9b285b2137f522634f7ca37872f2420ebb675
rikbellens <rik.bellens@appsup.be>
2022-12-30 18:30:39 +0100

    docs: add docs to Authenticators

--- lib/openid_client_browser.dart
+++ lib/openid_client_browser.dart
@@ -3,9 +3,33 @@ import 'dart:html' hide Credential, Client;
 import 'dart:async';
 export 'openid_client.dart';
 
+/// A wrapper around [Flow] that handles the browser-specific parts of
+/// authentication.
+///
+/// The constructor takes a [Client] and a list of scopes. It then
+/// creates a [Flow] and uses it to generate an authentication URI.
+///
+/// The [authorize] method redirects the browser to the authentication URI.
+///
+/// The [logout] method redirects the browser to the logout URI.
+///
+/// The [credential] property returns a [Future] that completes with a
+/// [Credential] after the user has signed in and the browser is redirected to
+/// the app. Otherwise, it completes with `null`.
+///
+/// The state is not persisted in the browser, so the user will have to sign in
+/// again after a page refresh. If you want to persist the state, you'll have to
+/// store and restore the credential yourself. You can listen to the
+/// [Credential.onTokenChanged] event to be notified when the credential changes.
 class Authenticator {
+  /// The [Flow] used for authentication.
+  ///
+  /// This will be a flow of type [FlowType.implicit].
   final Flow flow;
 
+  /// A [Future] that completes with a [Credential] after the user has signed in
+  /// and the browser is redirected to the app. Otherwise, it completes with
+  /// `null`.
   final Future<Credential?> credential;
 
   Authenticator._(this.flow) : credential = _credentialFromUri(flow);
@@ -17,11 +41,13 @@ class Authenticator {
           ..scopes.addAll(scopes)
           ..redirectUri = Uri.parse(window.location.href).removeFragment());
 
+  /// Redirects the browser to the authentication URI.
   void authorize() {
     window.localStorage['openid_client:state'] = flow.state;
     window.location.href = flow.authenticationUri.toString();
   }
 
+  /// Redirects the browser to the logout URI.
   void logout() async {
     var c = await credential;
     if (c == null) return;
--- lib/openid_client_io.dart
+++ lib/openid_client_io.dart
@@ -8,18 +8,36 @@ import 'openid_client.dart';
 
 export 'openid_client.dart';
 
+/// A wrapper around [Flow] that handles authentication in a non-web environment.
+///
+/// This authenticator uses a local http server to listen for the redirect from
+/// the authorization server. The server is started when the [authorize] method
+/// is called and stopped when the [cancel] method is called or when the
+/// authentication flow is completed.
+///
+/// Some authorization servers might not allow to redirect to a local http
+/// server. In that case, you should capture the authentication response in a
+/// different way and pass it to the [processResult] method.
 class Authenticator {
+  /// The [Flow] used for authentication.
   final Flow flow;
 
   final Function(String url) urlLancher;
 
+  /// The port used by the local http server.
   final int port;
 
+  /// The html content to display when the authentication flow is completed.
+  ///
+  /// If this is null, the [redirectMessage] will be displayed instead.
   final String? htmlPage;
 
-  /// Not working if html is not null
+  /// The text message to display when the authentication flow is completed.
+  ///
+  /// If [htmlPage] is not null, this will be ignored.
   final String? redirectMessage;
 
+  /// Creates an authenticator that uses the given [flow].
   Authenticator.fromFlow(
     this.flow, {
     Function(String url)? urlLancher,
@@ -33,6 +51,8 @@ class Authenticator {
         port = flow.redirectUri.port,
         urlLancher = urlLancher ?? _runBrowser;
 
+  /// Creates an authenticator that uses a [Flow.authorizationCodeWithPKCE] flow
+  /// when [redirectUri] is null and a [Flow.authorizationCode] flow otherwise.
   Authenticator(
     Client client, {
     this.port = 3000,
@@ -46,10 +66,19 @@ class Authenticator {
           'You can only use one variable htmlPage (give entire html) or redirectMessage (only string message)',
         ),
         redirectMessage = redirectMessage ?? 'You can now close this window',
-        flow = redirectUri == null ? Flow.authorizationCodeWithPKCE(client) : Flow.authorizationCode(client)
+        flow = redirectUri == null
+            ? Flow.authorizationCodeWithPKCE(client)
+            : Flow.authorizationCode(client)
           ..scopes.addAll(scopes)
           ..redirectUri = redirectUri ?? Uri.parse('http://localhost:$port/');
 
+  /// Starts the authentication flow.
+  ///
+  /// This method will start a local http server and open the authorization
+  /// server's authentication page in a browser.
+  ///
+  /// The server will be stopped when the [cancel] method is called or when the
+  /// authentication flow is completed.
   Future<Credential> authorize() async {
     var state = flow.authenticationUri.queryParameters['state']!;
 
@@ -62,7 +91,13 @@ class Authenticator {
     return flow.callback(response);
   }
 
-  /// cancel the ongoing auth flow, i.e. when the user closed the webview/browser without a successful login
+  /// Cancels the authentication flow.
+  ///
+  /// This method will stop the local http server and complete the [authorize]
+  /// method with an error.
+  ///
+  /// This method should be called when the user cancels the authentication flow
+  /// in the browser.
   Future<void> cancel() async {
     final state = flow.authenticationUri.queryParameters['state'];
     _requestsByState[state!]?.completeError(Exception('Flow was cancelled'));
@@ -71,38 +106,47 @@ class Authenticator {
   }
 
   static final Map<int, Future<HttpServer>> _requestServers = {};
-  static final Map<String, Completer<Map<String, String>>> _requestsByState = {};
+  static final Map<String, Completer<Map<String, String>>> _requestsByState =
+      {};
 
-  static Future<HttpServer> _startServer(int port, String? htmlPage, String? redirectMessage) {
-    return _requestServers[port] ??= (HttpServer.bind(InternetAddress.anyIPv4, port)
-      ..then((requestServer) async {
-        log('Server started at port $port');
-        await for (var request in requestServer) {
-          request.response.statusCode = 200;
-          if (redirectMessage != null) {
-            request.response.headers.contentType = ContentType.html;
-            request.response.writeln(htmlPage ??
-                '<html>'
-                    '<h1>$redirectMessage</h1>'
-                    '<script>window.close();</script>'
-                    '</html>');
-          }
-          await request.response.close();
-          var result = request.requestedUri.queryParameters;
+  static Future<HttpServer> _startServer(
+      int port, String? htmlPage, String? redirectMessage) {
+    return _requestServers[port] ??=
+        (HttpServer.bind(InternetAddress.anyIPv4, port)
+          ..then((requestServer) async {
+            log('Server started at port $port');
+            await for (var request in requestServer) {
+              request.response.statusCode = 200;
+              if (redirectMessage != null) {
+                request.response.headers.contentType = ContentType.html;
+                request.response.writeln(htmlPage ??
+                    '<html>'
+                        '<h1>$redirectMessage</h1>'
+                        '<script>window.close();</script>'
+                        '</html>');
+              }
+              await request.response.close();
+              var result = request.requestedUri.queryParameters;
 
-          if (!result.containsKey('state')) continue;
-          await processResult(result);
-        }
+              if (!result.containsKey('state')) continue;
+              await processResult(result);
+            }
 
-        await _requestServers.remove(port);
-      }));
+            await _requestServers.remove(port);
+          }));
   }
 
-  /// Process the Result from a auth Request
-  /// You can call this manually if you are redirected to the app by an external browser
+  /// Processes the result from an authentication flow.
+  ///
+  /// You can call this manually if you are redirected to the app by an external
+  /// browser.
+  ///
+  /// This method will complete the [authorize] method with the result of the
+  /// authentication flow.
   static Future<void> processResult(Map<String, String> result) async {
-    var r = _requestsByState.remove(result['state'])!;
-    r.complete(result);
+    var r = _requestsByState.remove(result['state']);
+    r?.complete(result);
+
     if (_requestsByState.isEmpty) {
       for (var s in _requestServers.values) {
         await (await s).close();
@@ -124,7 +168,8 @@ void _runBrowser(String url) {
       Process.run('explorer', [url]);
       break;
     default:
-      throw UnsupportedError('Unsupported platform: ${Platform.operatingSystem}');
+      throw UnsupportedError(
+          'Unsupported platform: ${Platform.operatingSystem}');
   }
 }
 
