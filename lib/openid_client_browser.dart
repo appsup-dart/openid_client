a4ac2c5095859e2e78308366cbc270d628420c4d
rikbellens <rik.bellens@appsup.be>
2022-12-30 15:32:09 +0100

    fix: initial token persisted in browser Authenticator

--- lib/openid_client_browser.dart
+++ lib/openid_client_browser.dart
@@ -1,7 +1,6 @@
 import 'openid_client.dart';
 import 'dart:html' hide Credential, Client;
 import 'dart:async';
-import 'dart:convert';
 export 'openid_client.dart';
 
 class Authenticator {
@@ -11,21 +10,20 @@ class Authenticator {
 
   Authenticator._(this.flow) : credential = _credentialFromUri(flow);
 
-  Authenticator(Client client, {Iterable<String> scopes = const []})
+  Authenticator(Client client,
+      {Iterable<String> scopes = const [], String? device})
       : this._(Flow.implicit(client,
-            state: window.localStorage['openid_client:state'])
+            device: device, state: window.localStorage['openid_client:state'])
           ..scopes.addAll(scopes)
           ..redirectUri = Uri.parse(window.location.href).removeFragment());
 
   void authorize() {
-    _forgetCredentials();
     window.localStorage['openid_client:state'] = flow.state;
     window.location.href = flow.authenticationUri.toString();
   }
 
   void logout() async {
     var c = await credential;
-    _forgetCredentials();
     if (c == null) return;
     var uri = c.generateLogoutUrl(
         redirectUri: Uri.parse(window.location.href).removeFragment());
@@ -34,29 +32,15 @@ class Authenticator {
     }
   }
 
-  void _forgetCredentials() {
-    window.localStorage.remove('openid_client:state');
-    window.localStorage.remove('openid_client:auth');
-  }
-
   static Future<Credential?> _credentialFromUri(Flow flow) async {
-    Map? q;
-    if (window.localStorage.containsKey('openid_client:auth')) {
-      q = json.decode(window.localStorage['openid_client:auth']!);
-    } else {
-      var uri = Uri(query: Uri.parse(window.location.href).fragment);
-      q = uri.queryParameters;
-      if (q.containsKey('access_token') ||
-          q.containsKey('code') ||
-          q.containsKey('id_token')) {
-        window.localStorage['openid_client:auth'] = json.encode(q);
-        window.location.href =
-            Uri.parse(window.location.href).removeFragment().toString();
-      }
-    }
-    if (q!.containsKey('access_token') ||
+    var uri = Uri(query: Uri.parse(window.location.href).fragment);
+    var q = uri.queryParameters;
+    if (q.containsKey('access_token') ||
         q.containsKey('code') ||
         q.containsKey('id_token')) {
+      window.history.replaceState(
+          '', '', Uri.parse(window.location.href).removeFragment().toString());
+      window.localStorage.remove('openid_client:state');
       return await flow.callback(q.cast());
     }
     return null;
