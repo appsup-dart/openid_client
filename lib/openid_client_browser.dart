54b6de8fe9e6044b777326f98c7d2e1b46a91f9e
Wahab Alshahin <wahab.alshahin@gmail.com>
2023-08-10 00:57:11 -0400

    Use universal_html package instead of dart:html
    Relax http package requirements

--- lib/openid_client_browser.dart
+++ lib/openid_client_browser.dart
@@ -1,5 +1,5 @@
 import 'openid_client.dart';
-import 'dart:html' hide Credential, Client;
+import 'package:universal_html/html.dart' hide Credential, Client;
 import 'dart:async';
 export 'openid_client.dart';
 
